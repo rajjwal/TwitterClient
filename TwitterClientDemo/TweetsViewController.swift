20ceae8c957ed3fee67c260c27ad0edf88778c0b
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2017-02-28 05:26:15 -0500

    Adjusted Autolayout settings

--- TwitterClientDemo/TweetsViewController.swift
+++ TwitterClientDemo/TweetsViewController.swift
@@ -24,6 +24,8 @@ class TweetsViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
         
         
